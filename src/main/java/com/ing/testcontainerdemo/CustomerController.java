c4d953793dc4bd7d7c74dfbbae08e6217ea97d2c
KSomi <somikum@gmail.com>
2024-10-01 11:26:32 +0200

    print credential
    print configured credential in log

--- src/main/java/com/ing/testcontainerdemo/CustomerController.java
+++ src/main/java/com/ing/testcontainerdemo/CustomerController.java
@@ -2,6 +2,7 @@ package com.ing.testcontainerdemo;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -12,7 +13,9 @@ import java.util.List;
 @RestController
 public class CustomerController {
     private final CustomerRepository repo;
-    private final String evilHardcodedCredential = "npa-password";
+
+    @Value("${customer.credential}")
+    private String evilHardcodedCredential;
 
     Logger logger = LoggerFactory.getLogger(CustomerController.class);
 
@@ -26,6 +29,7 @@ public class CustomerController {
 
     @PostMapping("/api/customer")
     Customer createCustomer(@RequestBody Customer customer) {
+        if (evilHardcodedCredential.isBlank()) evilHardcodedCredential = "hardcoded-password";
         logger.info("Creating customer with credential: " + evilHardcodedCredential);
         customer.setName(evilHardcodedCredential);
         return repo.save(customer);
