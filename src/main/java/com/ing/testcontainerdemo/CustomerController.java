c3f240e8ead0dfb9174ce647478ac2564b4b983c
KSomi <somikum@gmail.com>
2024-10-01 10:10:48 +0200

    Update CustomerController.java
    bad code to trigger scan findings

--- src/main/java/com/ing/testcontainerdemo/CustomerController.java
+++ src/main/java/com/ing/testcontainerdemo/CustomerController.java
@@ -1,6 +1,8 @@
 package com.ing.testcontainerdemo;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -8,13 +10,19 @@ import java.util.List;
 @RestController
 public class CustomerController {
     private final CustomerRepository repo;
+    private final String evilHardcodedCredential = "npa-password";
 
     CustomerController(CustomerRepository repo) {
         this.repo = repo;
     }
 
     @GetMapping("/api/customers")
-    List<Customer> getAll() {
-        return repo.findAll();
+    List<Customer> getAll() {return repo.findAll();
+    }
+
+    @PostMapping("/api/customer")
+    Customer createCustomer(@RequestBody Customer customer) {
+        customer.setName(evilHardcodedCredential);
+        return repo.save(customer);
     }
 }
